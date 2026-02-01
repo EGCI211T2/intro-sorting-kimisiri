1da3a89c3c7c654fb3c5c75cf8db532caee5ff19
xor ia <siri.nga@student.mahidol.edu>
2026-02-01 20:42:59 +0700

    whatever

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
-//
-//  main.cpp
-//  First arg
-//
-//  Created by Mingmanas Sivaraksa on 2/1/2567 BE.
-//
-
 #include <iostream>
+#include "sorting.h"
+#include <cstdio>
+#include <stdlib.h>
 using namespace std;
 
 int main(int argc, char * argv[]) {
-    
- 
-  cout<<"argc is "<<argc<<endl;
-    int i,N;
-    int *a;
-    for(i=1;i<argc;i++){
-        cout<<"argv[" << i<<"]" <<argv[i]<<endl;
-      
-    }
-    /* Convert to numbers*/
-    N=argc-1;
-    cout<<"Before sorting:";
-    display(a,N);
-    
-    sort(a,N);
-    cout<<"After sorting:";
-    display(a,N);
-    
-    
-    return 0;
+  int N;
+  N=argc-1;
+
+  int *a = new int[N];
+  for(int i=1; i < argc; i++){
+    a[i-1] = atoi(argv[i]);
+  }
+
+  printf("Before sorting : ");
+  display(a,N);
+
+  sort(a,N);
+
+  printf("After sorting : ");
+  display(a,N);
+  
+  delete[] a;
+  return 0;
 }
--- sorting.h
+++ sorting.h
@@ -1,3 +1,28 @@
-void swap(int &a,int &b);
-void sort(int *a,int N);
-void display(int *a,int N);
+#include <cstdio>
+
+void swap(int &a,int &b) {
+  int tmp = a;
+  a = b;
+  b = tmp;
+}
+
+void sort(int *a,int N) {
+  for (int i = 0; i < N-1; i++) {
+    for (int j = i+1; j < N; j++) {
+      if (a[i] > a[j]) {
+        swap(a[i], a[j]);
+      }
+    }
+  }
+}
+
+void display(int *a,int N) {
+  printf("[");
+  for (int i = 0; i < N; i++) {
+    printf("%d", a[i]);
+    if (i != (N-1)) {
+      printf(", ");
+    }
+  }
+  printf("]\n");
+}
